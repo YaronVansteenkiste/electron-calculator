662414887e14c2370b082fa6377c9fc42c74b114
yaronvsk <yaronvsk@googlemail.com>
2023-05-21 11:57:51 +0200

    21/05: simplified (array method)+ let->const

--- src/render.js
+++ src/render.js
@@ -1,68 +1,68 @@
 const closeBtn = document.getElementById('close-btn');
-let result = document.getElementById('result');
-let delEl = document.getElementById('del');
-let addEl = document.getElementById('+');
-let minusEl = document.getElementById('-');
-let eqEl =  document.getElementById('equation');
-let equalsEl = document.getElementById('equals');
-let plusNumb1 = 0;
+const result = document.getElementById('result');
+const delEl = document.getElementById('del');
+const addEl = document.getElementById('+');
+const minusEl = document.getElementById('-');
+const eqEl = document.getElementById('equation');
+const equalsEl = document.getElementById('equals');
 let prevNumber = 0;
 let currentOperator = '';
-let plusMinusEl = document.getElementById('plusMinus');
-let commaEl = document.getElementById('comma');
+const plusMinusEl = document.getElementById('plusMinus');
+const commaEl = document.getElementById('comma');
 
 delEl.onclick = removeNumber;
-addEl.onclick = function() { operatorFunction('+'); };
-minusEl.onclick = function() { operatorFunction('-'); };
+addEl.onclick = () => operatorFunction('+');
+minusEl.onclick = () => operatorFunction('-');
 equalsEl.onclick = equals;
 plusMinusEl.onclick = plusMinus;
 commaEl.onclick = addComma;
 
-for (let i = 0; i <= 9; i++) {
-    let num = document.getElementById(i.toString());
-    num.onclick = displayNumber;
-}
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.forEach(num => {
+const element = document.getElementById(num.toString());
+element.onclick = displayNumber;
+});
 
 function displayNumber() {
-    return result.innerText === '0' ? result.innerText = this.innerHTML : result.innerText += this.innerHTML;
+result.innerText = result.innerText === '0' ? this.innerHTML : result.innerText + this.innerHTML;
 }
 
 function removeNumber() {
-    if (result.innerText.length===1) {
-        result.innerText = '0';
-    } else {
-        result.innerText = result.innerText.substring(0, result.innerText.length - 1)
-    }
+if (result.innerText.length === 1) {
+result.innerText = '0';
+} else {
+result.innerText = result.innerText.slice(0, -1);
+}
 }
 
 function plusMinus() {
-    result.innerText = parseFloat(result.innerText) * -1;
+result.innerText = parseFloat(result.innerText) * -1;
 }
 
 function operatorFunction(operator) {
-    prevNumber = result.innerText;
-    eqEl.innerText = prevNumber + operator;
-    currentOperator = operator;
-    result.innerText = '0';
+prevNumber = result.innerText;
+eqEl.innerText = prevNumber + operator;
+currentOperator = operator;
+result.innerText = '0';
 }
 
 function equals() {
-    if (eqEl.textContent.includes('+')) {
-        result.innerText = parseFloat(prevNumber) + parseFloat(result.innerText);
-    } else if (eqEl.textContent.includes('-')) {
-        result.innerText = parseFloat(prevNumber) - parseFloat(result.innerText);
-    }
-    eqEl.innerText = '';
-    prevNumber = 0;
-    currentOperator = '';
+if (eqEl.textContent.includes('+')) {
+result.innerText = parseFloat(prevNumber) + parseFloat(result.innerText);
+} else if (eqEl.textContent.includes('-')) {
+result.innerText = parseFloat(prevNumber) - parseFloat(result.innerText);
+}
+eqEl.innerText = '';
+prevNumber = 0;
+currentOperator = '';
 }
 
 function addComma() {
-    if (!result.innerText.includes('.')) {
-        result.innerText += '.';
-    }
+if (!result.innerText.includes('.')) {
+result.innerText += '.';
+}
 }
 
 closeBtn.addEventListener('click', () => {
-  window.close();
+window.close();
 });
